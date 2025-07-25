23f872355e1e33456f7d5ffef33102f66db00cd4
Oswaldo Cardozo <odcg@hotmail.es>
2025-07-25 01:44:30 +0100

    Add files via upload

--- js/news.js
+++ js/news.js
@@ -15,12 +15,25 @@ entradas.forEach(el => observer.observe(el));
 
 /*SHOW THE TEXT */
 function toggleNews(element) {
-    const texto = element.querySelector(".news-text");
-    if (texto.style.display === "block") {
-        texto.style.display = "none";
-        } else {
-        texto.style.display = "block";
-    }
+  const texto = element.querySelector(".news-text");
+  if (texto.style.display === "block") {
+    texto.style.display = "none";
+    } else {
+    texto.style.display = "block";
+  }
+}
+
+
+// SLIDER BUTTON
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".slide");
+  const buttons = document.querySelectorAll(".slider-buttons .btn");
+
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+    buttons[i].classList.toggle("active", i === index);
+  });
 }
 
 
